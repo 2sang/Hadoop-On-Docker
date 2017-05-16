3658056a3249bff0728b1b6810ac7c2f341341dd
Sangsu Lee <sangsulee92@gmail.com>
2017-05-16 23:53:24 +0900

    almost done...

--- dockerfile-master/init.sh
+++ dockerfile-master/init.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 # init.sh
 
-# This enables execute runtime command at once,
+# This enables execute runtime command at once, and runs in container.
 # just list all needed commands below.
 
 
+# Add (ip, 'master') set in /etc/hosts
+echo $(hostname --ip-address) master >> /etc/hosts
 
+# keygen
+ssh-keygen -t rsa -N "" -f /root/.ssh/id_rsa
+
+# for every single nodes,
+
+for ((c=0; c<=$1; c++))
+do
+	ssh-keyscan -H slave$c >> ~/.ssh/known_hosts
+	sshpass -f /password.txt ssh-copy-id slave$c
+done
 
 # last target command
-/usr/sbin/sshd -D
+#/usr/sbin/sshd -D
